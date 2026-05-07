e0915258e1ee7b566596de4d330f6ef68f71a34a
Joost Soeterbroek <joost.soeterbroek@gmail.com>
2026-05-07 13:31:02 +0200

    Add NHA pipeline test wrapper

--- data-pipeline/tests/test_all.sh
+++ data-pipeline/tests/test_all.sh
@@ -7,6 +7,7 @@ cd /Users/lux/data-pipeline
 
 /bin/bash /Users/jsoeterbroek/Development/nlux-project/nlux/data-pipeline/tests/test_fhm.sh
 /bin/bash /Users/jsoeterbroek/Development/nlux-project/nlux/data-pipeline/tests/test_hvh.sh
+/bin/bash /Users/jsoeterbroek/Development/nlux-project/nlux/data-pipeline/tests/test_nha.sh
 /bin/bash /Users/jsoeterbroek/Development/nlux-project/nlux/data-pipeline/tests/test_rbhc.sh
 /bin/bash /Users/jsoeterbroek/Development/nlux-project/nlux/data-pipeline/tests/test_teylers.sh
 /bin/bash /Users/jsoeterbroek/Development/nlux-project/nlux/data-pipeline/tests/test_wfm.sh
