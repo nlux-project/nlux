39c46f476faee66214969dc0b5a1a9a1b606bd90
Joost Soeterbroek <joost.soeterbroek@gmail.com>
2026-05-18 14:43:11 +0200

    Refactor shared NHA mapper

--- data-pipeline/pipeline/sources/museums/nha/c1477/mapper.py
+++ data-pipeline/pipeline/sources/museums/nha/c1477/mapper.py
@@ -1,10 +1,10 @@
-from pipeline.sources.museums.nha.c587.mapper import NHA_DETAIL_BASE, NHA_LABEL, NhaC587Mapper
+from pipeline.sources.museums.nha.mapper import NHA_DETAIL_BASE, NHA_LABEL, NhaMapper
 
 
 NHA_COLLECTION_LABEL = "1477 - prenten van C.G. Voorhelm Schneevoogt te Haarlem"
 
 
-class NhaC1477Mapper(NhaC587Mapper):
+class NhaC1477Mapper(NhaMapper):
     source_name = "nha-c1477"
     owner_label = NHA_LABEL
     collection_label = NHA_COLLECTION_LABEL
--- data-pipeline/pipeline/sources/museums/nha/c480/mapper.py
+++ data-pipeline/pipeline/sources/museums/nha/c480/mapper.py
@@ -1,10 +1,10 @@
-from pipeline.sources.museums.nha.c587.mapper import NHA_DETAIL_BASE, NHA_LABEL, NhaC587Mapper
+from pipeline.sources.museums.nha.mapper import NHA_DETAIL_BASE, NHA_LABEL, NhaMapper
 
 
 NHA_COLLECTION_LABEL = "480 - historieprenten van de Provinciale Atlas Noord-Holland"
 
 
-class NhaC480Mapper(NhaC587Mapper):
+class NhaC480Mapper(NhaMapper):
     source_name = "nha-c480"
     owner_label = NHA_LABEL
     collection_label = NHA_COLLECTION_LABEL
